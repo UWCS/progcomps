204fc26405c4c9106e640dbe722854491aeab2d0
Levelent <31184681+Levelent@users.noreply.github.com>
2022-02-16 18:59:37 +0000

    updated gen function

--- 2022-02-16/2/gen.py
+++ 2022-02-16/2/gen.py
@@ -1,6 +1,6 @@
 from random import randint
 
-print(10000)
+print(5000)
 
 print(1, 9)
 print(1, 100)
@@ -8,5 +8,5 @@ print(4, 13)
 print(0, 100)
 print(1000, 0)
 
-for i in range(9995):
+for i in range(5000, 9995):
     print(randint(0, (i // 10) + 5), randint(0, (i // 100) + 5))
